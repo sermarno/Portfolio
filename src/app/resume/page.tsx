132b800d1ace29e2f81ff2408ed53c4e63272b11
Serra Arnold <sermarno@iu.edu>
2025-10-13 12:33:48 -0400

    About page styling changes

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,60 +1,60 @@
 "use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import projects from "@/data/projects";
-import { Github } from "lucide-react";
-import { FileText } from "lucide-react";
-import { ExternalLink, Eye } from "lucide-react";
+
+import { ExternalLink, Eye, FileText, Folder, Github } from "lucide-react";
 
 export default function ResumePage() {
   return (
-    <main className="py-5 bg-[#2e2e2e] rounded-xl m-3">
-      <div className="relative flex items-center w-full px-4">
-        <h1 className="absolute left-1/2 transform -translate-x-1/2 text-3xl">
-          Serra Arnold
-        </h1>
-        <a
-          href="/Resume.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="ml-auto inline-flex items-center text-white px-4 py-2 rounded-lg border border-grey hover:bg-gray-700 transition"
-        >
-          <FileText size={28} className="mr-2" />
-          View Resume
-        </a>
-      </div>
+    <main>
+      <div className="max-w-5xl mx-auto px-6 py-5 bg-[#2e2e2e] rounded-xl m-3">
+        <div className="relative flex items-center w-full px-4">
+          <h1 className="absolute left-1/2 transform -translate-x-1/2 text-3xl">
+            Serra Arnold
+          </h1>
+          <a
+            href="/Resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-auto inline-flex items-center text-white bg-[#4a4a4a] px-4 py-2 rounded-lg border border-[#2a2a2a] hover:bg-[#3a3a3a] hover:-translate-y-1 hover:shadow-lg transition-all duration-300 ease-in-out"
+          >
+            <FileText size={28} className="mr-2" />
+            View Resume
+          </a>
+        </div>
 
-      {/* Contact Section */}
-      <section className="flex justify-center bg-[#2e2e2e] relative py-5">
-        <div className="max-w-6xl w-full">
-          <div className="flex justify-center space-x-10">
-            {/* GitHub */}
-            <a
-              href="https://github.com/sermarno"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex text-white text-sm items-center hover:text-gray-300 transition"
-            >
-              <Github size={28} />
-              <p className="px-1">GitHub</p>
-            </a>
-
-            {/* LinkedIn */}
-            <a
-              href="https://linkedin.com/in/serraarnold"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex text-white text-sm items-center hover:text-gray-300 transition"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="28"
-                height="28"
-                viewBox="0 0 24 24"
-                fill="currentColor"
+        {/* Contact Section */}
+        <section className="flex justify-center bg-[#2e2e2e] relative py-5">
+          <div className="max-w-6xl w-full">
+            <div className="flex justify-center space-x-10">
+              {/* GitHub */}
+              <a
+                href="https://github.com/sermarno"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex text-white text-sm items-center hover:bg-[#3a3a3a] hover:px-4 hover:py-2 hover:-translate-y-1 hover:shadow-lg transition-all duration-300 ease-in-out"
               >
-                <path
-                  d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 
+                <Github size={28} />
+                <p className="px-1">GitHub</p>
+              </a>
+
+              {/* LinkedIn */}
+              <a
+                href="https://linkedin.com/in/serraarnold"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex text-white text-sm items-center hover:bg-[#3a3a3a] hover:px-4 hover:py-2 hover:-translate-y-1 hover:shadow-lg transition-all duration-300 ease-in-out"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="28"
+                  height="28"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path
+                    d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 
     2.761 2.239 5 5 5h14c2.762 0 5-2.239 
     5-5v-14c0-2.761-2.238-5-5-5zm-11 
     19h-3v-10h3v10zm-1.5-11.268c-.966 
@@ -65,88 +65,99 @@ export default function ResumePage() {
     0-2.155 1.459-2.155 2.963v5.7h-3v-10h2.885v1.367h.041c.403-.763 
     1.387-1.566 2.853-1.566 3.051 0 3.621 
     2.008 3.621 4.621v5.578z"
-                />
-              </svg>{" "}
-              <p className="px-1">LinkedIn</p>
-            </a>
-            {/* Gmail */}
-            <a
-              href="mailto:serraarnold0@gmail.com"
-              className="flex text-white text-sm items-center hover:text-gray-300 transition"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="28"
-                height="28"
-                viewBox="0 0 24 24"
+                  />
+                </svg>{" "}
+                <p className="px-1">LinkedIn</p>
+              </a>
+              {/* Gmail */}
+              <a
+                href="mailto:serraarnold0@gmail.com"
+                className="flex text-white text-sm items-center hover:bg-[#3a3a3a] hover:px-4 hover:py-2 hover:-translate-y-1 hover:shadow-lg transition-all duration-300 ease-in-out"
               >
-                <path
-                  fill="currentColor"
-                  d="M12 12.713l-11.985-7.713v13.928c0 
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="28"
+                  height="28"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M12 12.713l-11.985-7.713v13.928c0 
       1.104.896 2 2 2h19.97c1.104 
       0 2-.896 2-2v-13.928l-11.985 
       7.713zm11.985-10.713h-23.97l11.985 
       7.713 11.985-7.713z"
-                />
-              </svg>
-              <p className="px-1">Email</p>
-            </a>
+                  />
+                </svg>
+                <p className="px-1">Email</p>
+              </a>
+            </div>
           </div>
-        </div>
-      </section>
-      {/* Main Content */}
-      <section className="m-9">
-        {/* Professional Summary */}
-        <h3 className="flex justify-center text-xl">Professional Summary</h3>
-        <div className="flex justify-center py-1">
-          <div className="flex-shrink-0">
-            <Image
-              src="/resumeImage.png"
-              alt="Self Portait"
-              width={250}
-              height={200}
-              className="object-cover rounded-xl border-2 border-pink-200 shadow-[0_0_30px_5px_rgba(255,105,180,0.6)]"
-            />
+        </section>
+        {/* Main Content */}
+        <section className="m-9">
+          {/* Professional Summary */}
+          <div className="flex justify-center py-4">
+            <div className="flex-shrink-0">
+              <Image
+                src="/resumeImg1.png"
+                alt="Self Portait"
+                width={250}
+                height={200}
+                className="object-cover rounded-xl border-2 border-pink-200 shadow-[0_0_30px_5px_rgba(255,105,180,0.6)]"
+              />
+            </div>
+            <div className="px-3 py-4 ml-9">
+              <h3 className="flex text-xl font-bold mb-2">
+                Professional Summary
+              </h3>
+              <p>
+                First-generation university graduate from IU with a B.S. in
+                Informatics and focused studies in web development and design.
+                Actively identifying ways to enhance existing user interfaces on
+                devices, improve digital intuition with modernized technology,
+                and utilize UX research and design principals to identify user
+                needs, build low to high-fidelity prototypes, and develop clean,
+                effective code for user-centered intuitive interfaces.
+              </p>
+              <a
+                href="/projects"
+                rel="noopener noreferrer"
+                className="inline-flex flex-col items-center justify-center text-white bg-[#fb5bbf] px-4 py-2 mt-3 rounded-lg hover:bg-[#d63d9d] hover:-translate-y-1 hover:shadow-lg transition-all duration-300 ease-in-out"
+              >
+                <Folder size={28} className="mb-1" />
+                View Projects
+              </a>
+            </div>
           </div>
-          <div className="px-3">
-            <p className="m-2">
-              First-generation university graduate from IU with a B.S. in
-              Informatics and focused studies in web development and design.
-              Actively identifying ways to enhance existing user interfaces on
-              devices, improve digital intuition with modernized technology, and
-              utilize UX research and design principals to identify user needs,
-              build low to high-fidelity prototypes, and develop clean,
-              effective code for user-centered intuitive interfaces.
-            </p>
+        </section>
+        <section className="m-10">
+          {/* Education Section */}
+          <div className="flex justify-between items-center gap-10">
+            <div className="flex-1">
+              <h3 className="text-xl font-bold mb-2">Education</h3>
+              <h3>Indiana University</h3>
+              <p className="italic">Bloomington, IN</p>
+              <p className="mt-2">
+                B.S. Informatics, <span className="italic">May 2025</span>
+              </p>
+              <ul className="list-disc list-inside mt-2 space-y-1">
+                <li>GPA: 3.54 Overall (Major GPA: 3.9)</li>
+                <li>Achievments: Dean's List - 3 Semesters</li>
+              </ul>
+            </div>
+            <div className="flex-shrink-0">
+              <Image
+                src="/resumeImg2.png"
+                alt="Luddy Grad Photo"
+                width={250}
+                height={200}
+                className="object-cover rounded-xl border-2 border-pink-200 shadow-[0_0_30px_5px_rgba(255,105,180,0.6)]"
+              />
+            </div>
           </div>
-        </div>
-      </section>
-      <section className="m-9">
-        <h3 className="text-center text-xl mb-2">Education</h3>
-        {/* Education Section */}
-        <div className="flex justify-between items-start gap-10">
-          <div className="flex-1">
-            <h3 className="text-lg">Indiana University</h3>
-            <p className="italic">Bloomington, IN</p>
-            <p className="mt-2">
-              B.S. Informatics, <span className="italic">May 2025</span>
-            </p>
-            <ul className="list-disc list-inside mt-2 space-y-1">
-              <li>GPA: 3.54 Overall (Major GPA: 3.9)</li>
-              <li>Achievments: Dean's List - 3 Semesters</li>
-            </ul>
-          </div>
-          <div className="flex-shrink-0">
-            <Image
-              src="/luddy.png"
-              alt="Luddy Grad Photo"
-              width={250}
-              height={200}
-              className="object-cover rounded-xl border-2 border-pink-200 shadow-[0_0_30px_5px_rgba(255,105,180,0.6)]"
-            />
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </main>
   );
 }
